deda35eea8f3412f9b0a7e4d6d4574f634ac590e
Christian Volkmann <christian.volkmann@rwth-aachen.de>
2021-02-16 04:11:13 +0100

    fix: Bash installer

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ _CODE_CONNECT_PY=$(realpath ~)/.code-connect/code_connect.py
 echo "> Downloading code_connect.py from https://raw.githubusercontent.com/chvolkmann/code-connect/main/functions/code_connect.py"
 mkdir -p $(dirname $_CODE_CONNECT_PY)
 wget -q -O "$_CODE_CONNECT_PY" "https://raw.githubusercontent.com/chvolkmann/code-connect/main/functions/code_connect.py"
+chmod +x "$_CODE_CONNECT_PY"
 
 # Add the alias to ~/.bashrc if not already done
 if [[ -z $(cat ~/.bashrc | grep "alias code=") ]]; then
