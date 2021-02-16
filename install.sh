23245a65a80c52fc842ed5298d84719e44a0e130
Christian Volkmann <christian.volkmann@rwth-aachen.de>
2021-02-16 04:06:37 +0100

    fix: Fix bash installer

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Fetch code_connect.py
-mkdir -p ~/.code-connect
-wget https://raw.githubusercontent.com/chvolkmann/code-connect/main/functions/code_connect.py
+_CODE_CONNECT_PY=$(realpath ~)/.code-connect/code_connect.py
+
+echo "> Downloading code_connect.py from https://raw.githubusercontent.com/chvolkmann/code-connect/main/functions/code_connect.py"
+mkdir -p $(dirname $_CODE_CONNECT_PY)
+wget -q -O "$_CODE_CONNECT_PY" "https://raw.githubusercontent.com/chvolkmann/code-connect/main/functions/code_connect.py"
 
 # Add the alias to ~/.bashrc if not already done
-if [[ -z $(cat ~/.bashrc | grep "alias codes=") ]]; then
-    echo 'alias code="/path/to/code_connect.py"' >> ~/.bashrc
+if [[ -z $(cat ~/.bashrc | grep "alias code=") ]]; then
+    echo "> Alias added to ~/.bashrc"
+    echo "alias code='$_CODE_CONNECT_PY'" >> ~/.bashrc
+else
+    echo "> code already aliased in ~/.bashrc, skipping"
 fi
 
 # Register the alias in the current shell
-alias code="/path/to/code_connect.py"
+alias code="$_CODE_CONNECT_PY"
+
+echo "> code-connect installed successfully!"
+echo ">"
+echo "> You can now use the code command, try it!"
