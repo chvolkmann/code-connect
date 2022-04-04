f68f15fd14aa08c93097cdad46725359615e6605
Andrew Freckelton <7751633+frecks@users.noreply.github.com>
2022-04-04 14:55:33 -0400

    Fish shell doesn't use colon termination for `if`
    This commit removes the python style colon line termination on the if
    statement. The colon was causing undesired behavior when executing this
    function in the fish shell. Reference the fish shell docs
    [example](https://fishshell.com/docs/current/cmds/if.html#example) of an
    `if` statement for proper syntax.

--- functions/code.fish
+++ functions/code.fish
@@ -4,7 +4,7 @@
 
 function code --description 'Run local code exectuable if installed, run code-connect otherwise'
     set -l local_code_executable (which code)
-    if test -n "$local_code_executable":
+    if test -n "$local_code_executable"
         # code is in the PATH, use that binary instead of the code-connect
         $local_code_executable $argv
     else
