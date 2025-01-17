922fd2358db5d9341a5b3819bfec4caf1ce3d0be
ELLIOTTCABLE <git@ell.io>
2025-01-17 16:46:01 -0600

    (- fix sh) Fix quoting in scripts; mark as POSIX-sh

--- bash/code-connect.sh
+++ bash/code-connect.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # https://github.com/chvolkmann/code-connect
 
 # Use this script through an alias
 #   alias code-connect="/path/to/code-connect.sh"
 
-~/.code-connect/bin/code_connect.py $@
+code_connect_dir=$(CDPATH='' cd -- "$(dirname -- "$(dirname -- "$0")")" && pwd)
+"$code_connect_dir/bin/code_connect.py" "$@"
--- bash/code.sh
+++ bash/code.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 # https://github.com/chvolkmann/code-connect
 
@@ -8,8 +8,9 @@
 local_code_executable="$(which code 2>/dev/null)"
 if test -n "$local_code_executable"; then
     # code is in the PATH, use that binary instead of the code-connect
-    $local_code_executable $@
+    "$local_code_executable" "$@"
 else
     # code not locally installed, use code-connect
-    ~/.code-connect/bin/code_connect.py $@
+    code_connect_dir=$(CDPATH='' cd -- "$(dirname -- "$(dirname -- "$0")")" && pwd)
+    "$code_connect_dir/bin/code_connect.py" "$@"
 fi
